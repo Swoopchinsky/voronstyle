9968e49390c670d5dcb43d4bf4bd41c4519f5832
swoopchinsky <parkerswoop93@mail.ru>
2025-11-25 20:04:02 +0300

    udacha v.1.1.0.2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Romch from "./components/Romch";
 
+import { useEffect, useState } from "react";
+
+export function useIsMobile(MOBILE_BREAKPOINT = 768) {
+  const [isMobile, setIsMobile] = useState(undefined);
+
+  useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+
+    mql.addEventListener("change", onChange);
+
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+
+    return () => {
+      mql.removeEventListener("change", onChange);
+    };
+
+  }, [MOBILE_BREAKPOINT]);
+
+  return !!isMobile;
+}
+
+
 function App() {
   return (
     <div className="osnova">
