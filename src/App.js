6e5ab64fdaaa9ed4708d96cb3cd6e4e1d0c2119f
swoopchinsky <parkerswoop93@mail.ru>
2025-11-30 01:06:59 +0300

    udacha v.1.1.1.8

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Romch from "./components/Romch";
+import Slayder from "./components/Slayder";
 
 
 
@@ -9,6 +10,7 @@ function App() {
     <div className="osnova">
       <Header />
       <Romch />
+      <Slayder />
       <Footer />
 
 
---
+++ src/components/Slayder.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Carousel } from '../carousel/Carousel'
+
+
+export default function Slayder() {
+    return (
+        <Carousel>
+            <div className='item item-1'>Item 1</div>
+            <div className='item item-2'>Item 2</div>
+            <div className='item item-3'>Item 3</div>
+        </Carousel>
+    )
+}
