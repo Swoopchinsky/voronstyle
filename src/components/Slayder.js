c840bb960b9b025a422a422810f4ac531151c97e
swoopchinsky <parkerswoop93@mail.ru>
2025-12-04 12:52:24 +0300

    udacha v.1.1.1.10

--- src/components/Slayder.js
+++ src/components/Slayder.js
@@ -8,6 +8,16 @@ export default function Slayder() {
             <div className='item item-1'>Item 1</div>
             <div className='item item-2'>Item 2</div>
             <div className='item item-3'>Item 3</div>
+
+            <div className='item item-4'>swoopchinsky</div>
+            <div className='item item-5'>romch</div>
+            <div className='item item-6'>serzh</div>
+
+            <div className='item item-7'>shmoolking</div>
+            <div className='item item-8'>IIL</div>
+            <div className='item item-9'>youking</div>
+
+            <div className='item item-10'>Spas</div>
         </Carousel>
     )
 }
