71a6384b64748805b918e10bf900e21cba0d9690
swoopchinsky <parkerswoop93@mail.ru>
2025-12-09 13:14:06 +0300

    udacha v.1.1.1.17

--- src/snimok/Snimok.js
+++ src/snimok/Snimok.js
@@ -3,17 +3,37 @@ import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 
 import './Snimok.css'
 
-const KROK_WIDTH = 450
+const KROK_WIDTH = 1340
 
 export const Snimok = ({ children }) => {
     const [krok, setKrok] = useState([])
+    const [offset, setOffset] = useState(0)
+
 
     const leftClick = () => {
         console.log('leftClick')
+
+        setOffset(currentOffset => {
+            const newOffset = currentOffset + KROK_WIDTH
+
+            console.log(newOffset)
+            return Math.min(newOffset, 0)
+        })
     }
 
     const rightClick = () => {
         console.log('rightClick')
+
+        setOffset((currentOffset) => {
+
+            const newOffset = currentOffset - KROK_WIDTH
+
+            const maxOffset = -(KROK_WIDTH * (krok.length - 1))
+
+            console.log(newOffset, maxOffset)
+            return Math.max(newOffset, maxOffset)
+
+        })
     }
 
 
@@ -24,8 +44,8 @@ export const Snimok = ({ children }) => {
                 return cloneElement(child, {
                     style: {
                         height: '100%',
-                        ninWidth: `${KROK_WIDTH}px`,
-                        maxwidth: `${KROK_WIDTH}px`,
+                        minWidth: `${KROK_WIDTH}px`,
+                        maxWidth: `${KROK_WIDTH}px`,
                     },
                 })
             })
@@ -38,7 +58,14 @@ export const Snimok = ({ children }) => {
         <div className='snimok-container'>
             <FaChevronLeft className='snarrow' onClick={leftClick} />
             <div className='win-snimok'>
-                <div className="all-snimok-container">{krok}</div>
+                <div className='all-snimok-container'
+
+                    style={{
+                        transform: `translateX(${offset}px)`
+                    }}
+
+
+                >{krok}</div>
             </div>
             <FaChevronRight className='snarrow' onClick={rightClick} />
 
