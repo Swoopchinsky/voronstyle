f68db2f46e3ef21bcbeef3070d780bef88afdedb
swoopchinsky <parkerswoop93@mail.ru>
2026-01-01 16:04:20 +0300

    udacha tvorca

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,15 +7,19 @@ export default function Header() {
         <header>
             <div>
                 <span className='foto'>voronstyle</span>
+
                 <header className='header'>
-                    <button className='menu-button' onClick={() => setOpen(!isOpen)}></button>
+                    <button className='menu-button' onClick={() => setOpen(!isOpen)}>
+
+
+                    </button>
                     <nav className={`menu ${isOpen ? "active" : ""}`}>
                         <ul className='menu__list'>
-                            <li className='menu__list'>profile</li>
-                            <li className='menu__list'>bonus</li>
-                            <li className='menu__list'>notify</li>
-                            <li className='menu__list'>location</li>
-                            <li className='menu__list'>settion</li>
+                            <li className='menu__list'>web atcard</li>
+                            <li className='menu__list'>foto & designs</li>
+                            <li className='menu__list'>UX & UI</li>
+                            <li className='menu__list'>drawings</li>
+                            <li className='menu__list'>directions promising</li>
                             <li className='menu__list'>exit</li>
 
                         </ul>
